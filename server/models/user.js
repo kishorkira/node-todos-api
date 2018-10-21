9226c0123e9c21772fe1f8f809c8769b5eed1f1f
kishorkira <kishorkira@gmail.com>
2018-10-21 21:17:03 +0530

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -47,6 +47,15 @@ userSchema.methods.generateAuthToken = function(){
     user.tokens.push({access,token});
     return user.save().then(()=>token);
 };
+userSchema.methods.removeToken = function (token) {
+    var user = this;  
+    return user.update({
+      $pull: {
+        tokens: {token}
+      }
+    });
+};
+
 userSchema.statics.findByToken = function(token){
     let user = this;
     let decoded;
